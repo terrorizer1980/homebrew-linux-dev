d132b4dd7ce4a61cb2a3689c5ac9ddfa0c2e0fec
Shaun Jackman <sjackman@gmail.com>
2017-02-06 22:48:54 -0800

    Add new command test-bot-docker
    Build a bottle for the specified formulae using a Docker container.

---
+++ cmd/brew-test-bot-docker.rb
@@ -0,0 +1,31 @@
+#: `test-bot-docker` <formule>...
+#:
+#:  Build a bottle for the specified formulae using a Docker container.
+#:  See `test-bot` for further options accepted by this command.
+
+module Homebrew
+  module_function
+
+  def test_bot_docker
+    argv = ARGV.join(" ")
+    ENV["BINTRAY_USER"] ||= "linuxbrewtestbot"
+    safe_system "docker", "run", "--name=linuxbrew-test-bot",
+      "-e", "BINTRAY_USER", "-e", "BINTRAY_KEY",
+      "linuxbrew/linuxbrew",
+      "sh", "-c", "brew test-bot #{argv} && ls && brew test-bot --ci-upload && head *.json"
+
+    safe_system "docker", "cp", "linuxbrew-test-bot:/home/linuxbrew", "linuxbrew-test-bot"
+    cd "linuxbrew-test-bot" do
+      safe_system HOMEBREW_BREW_FILE, "bottle", "--merge", "--write", *Dir["*.json"]
+    end
+
+    oh1 "Done!"
+    puts <<-EOS.undent
+      To clean up, run
+        docker rm linuxbrew-test-bot
+        rm -rf linuxbrew-test-bot
+    EOS
+  end
+end
+
+Homebrew.test_bot_docker
