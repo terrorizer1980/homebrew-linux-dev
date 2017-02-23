416fcebdd90d7aa0227742295ab8bb9f8e0a980d
Shaun Jackman <sjackman@gmail.com>
2017-02-23 11:32:07 -0800

    squash-bottle-pr: Drop the --keep-old commit (#29)
    Drop the commit whose subject starts with "drop!" added by
    brew build-bottle-pr --keep-old

--- cmd/brew-squash-bottle-pr.rb
+++ cmd/brew-squash-bottle-pr.rb
@@ -7,6 +7,11 @@ module Homebrew
   #    brew pull --bottle 123
   #    brew squash-bottle-pr
   def squash_bottle_pr
+    unless Utils.popen_read("git", "log", "-n1", "--pretty=%s", "HEAD~1") =~ /: Build a bottle for Linuxbrew$/
+      opoo "No build-bottle-pr commit was found"
+      return
+    end
+
     head = `git rev-parse HEAD`.chomp
     formula = `git log -n1 --pretty=format:%s`.split(":").first
     file = Formula[formula].path
@@ -23,6 +28,12 @@ module Homebrew
     ENV["GIT_EDITOR"] = git_editor
 
     safe_system "git", "show" if ARGV.verbose?
+
+    if Utils.popen_read("git", "log", "-n1", "--pretty=%s", "HEAD~1") =~ /^drop! /
+      bottle_head = Utils.popen_read("git", "rev-parse", "HEAD").chomp
+      safe_system "git", "reset", "--hard", "HEAD~2"
+      safe_system "git", "cherry-pick", bottle_head
+    end
   end
 end
 
