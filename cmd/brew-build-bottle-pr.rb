7e48a30df5ca87eed2816c7f0403fe66ad25aa64
Shaun Jackman <sjackman@gmail.com>
2017-03-03 15:03:52 -0800

    build-bottle-pr: Remove --online from audit
    Brew audit --online can be very slow.

--- cmd/brew-build-bottle-pr.rb
+++ cmd/brew-build-bottle-pr.rb
@@ -96,7 +96,7 @@ module Homebrew
     @n += 1
     return ohai "#{formula}: Skipping because GitHub rate limits pull requests (limit = #{limit})." if @n > limit
 
-    system HOMEBREW_BREW_FILE, "audit", "--strict", "--online", formula.path
+    system HOMEBREW_BREW_FILE, "audit", "--strict", formula.path
     odie "Please fix audit failure for #{formula}" unless $?.success?
 
     message = "#{formula}: Build a bottle for Linuxbrew"
