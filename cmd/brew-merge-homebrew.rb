aef9f62b38b03365c3e7a183d68ce2f53d905db0
Jonathan Chang <jchang641@gmail.com>
2017-07-26 08:10:49 -0700

    merge-homebrew: remove defunct --science option (#48)

--- cmd/brew-merge-homebrew.rb
+++ cmd/brew-merge-homebrew.rb
@@ -1,9 +1,8 @@
-#:  * `merge-homebrew` [`--brew`|`--core`|`--science`] [<commit>]:
+#:  * `merge-homebrew` [`--brew`|`--core`] [<commit>]:
 #:   Merge branch homebrew/master into linuxbrew/master.
 #:
 #:   If `--brew` is passed, merge Homebrew/brew into Linuxbrew/brew.
 #:   If `--core` is passed, merge Homebrew/homebrew-core into Linuxbrew/homebrew-core.
-#:   If `--science` is passed, merge Homebrew/homebrew-science into Linuxbrew/homebrew-science.
 #:   If <commit> is passed, merge only up to that upstream SHA-1 commit.
 
 require "date"
@@ -79,76 +78,6 @@ module Homebrew
     end
   end
 
-  def merge_science
-    oh1 "Merging Homebrew/homebrew-science into Linuxbrew/homebrew-science"
-    cd Tap.fetch("homebrew/science").path
-
-    safe_system git, "fetch", "homebrew"
-    safe_system git, "pull", "--ff-only", "linuxbrew", "master"
-    files = Utils.popen_read(git, "diff", "--name-only", "homebrew/master").split
-    return if files.empty?
-
-    puts "Updated upstream: #{files.join(" ")}"
-    files.select! do |filename|
-      next true unless File.readable? filename
-      !File.read(filename)[/bottle :(disabled|unneeded)/]
-    end
-    unless files.empty?
-      log = Utils.popen_read(git, "log", "linuxbrew/master..homebrew/master", "--", *files)
-      issues = log.scan(/^    Closes #([0-9]*)\.$/).flatten.reverse
-    end
-    if issues.nil? || issues.empty?
-      git_merge fast_forward: true
-      oh1 "No bottles to update"
-      puts "Now run:\n  git push homebrew && git push linuxbrew"
-      return
-    end
-
-    urls = issues.map { |n| "https://github.com/Homebrew/homebrew-science/pull/#{n}" }
-    puts "Updating bottles: #{files.join(" ")}", "Pull requests: #{issues.join(" ")}", urls
-    bottle_commits = urls.flat_map do |url|
-      safe_system git, "checkout", "-B", "master", "linuxbrew/master"
-      system HOMEBREW_BREW_FILE, "pull", "--bottle", "--linux", "--resolve", url
-      while Utils.popen_read(git, "status").include? "You are in the middle of an am session."
-        conflicts = resolve_conflicts
-        if conflicts.empty?
-          opoo "Skipping empty patch"
-          system git, "am", "--skip"
-          next
-        end
-        system git, "am", "--continue"
-      end
-      logs = Utils.popen_read(git, "log", "--oneline", "linuxbrew/master..").split("\n")
-      commits = logs.map do |s|
-        s[/^([0-9a-f]+) .+: (add|update) .+ bottle for Linuxbrew\.$/, 1]
-      end.compact
-      puts "Bottle commits: #{commits.join(" ")}"
-      commits
-    end
-    safe_system git, "checkout", "-B", "master", "homebrew/master"
-
-    if bottle_commits.empty?
-      oh1 "No bottles to update"
-      puts "Now run:\n  git push homebrew && git push linuxbrew"
-      return
-    end
-
-    puts "Updated bottle commits: #{bottle_commits.join(" ")}"
-    system git, "cherry-pick", *bottle_commits
-    while Utils.popen_read(git, "status").include? "You are currently cherry-picking"
-      if resolve_conflicts.empty?
-        opoo "Skipping empty patch"
-        system git, "reset"
-      end
-      system git, "cherry-pick", "--continue"
-    end
-
-    safe_system git, "diff", "homebrew/master..master"
-    safe_system git, "log", "--oneline", "--decorate=short", "homebrew/master..master"
-    oh1 "Done"
-    puts "Now run:\n  git push homebrew && git push origin"
-  end
-
   def homebrew_commits
     if ARGV.named.empty?
       ["homebrew/master"]
@@ -160,11 +89,10 @@ module Homebrew
 
   def merge_homebrew
     Utils.ensure_git_installed!
-    repos = %w[--brew --core --science]
+    repos = %w[--brew --core]
     odie "Specify one of #{repos.join " "}" if (ARGV & repos).empty?
     merge_brew if ARGV.include? "--brew"
     merge_core if ARGV.include? "--core"
-    merge_science if ARGV.include? "--science"
   end
 end
 
