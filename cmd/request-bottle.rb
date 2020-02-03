62104a3d91a279d9691f24991bb7f8117cfa21e8
Issy Long <me@issyl0.co.uk>
2020-02-03 20:28:56 +0100

    Add cmd/request-bottle and delete old manual bottling scripts (#147)
    * Add cmd/request-bottle and delete old manual bottling scripts
    
    - Supersedes #146 to use Homebrew.args instead of ARGV.
    
    * request-bottle: last full_name
    
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ cmd/request-bottle.rb
@@ -0,0 +1,27 @@
+require "cli/parser"
+require "utils/github"
+
+module Homebrew
+  module_function
+
+  def request_bottle_args
+    Homebrew::CLI::Parser.new do
+      usage_banner <<~EOS
+        `request-bottle` <formula>
+        Build a bottle for this formula with GitHub Actions.
+      EOS
+    end
+  end
+
+  def request_bottle
+    request_bottle_args.parse
+
+    raise FormulaUnspecifiedError if Homebrew.args.named.empty?
+
+    formula = Homebrew.args.resolved_formulae.last.full_name
+
+    data = { event_type: "bottling", client_payload: { formula: formula } }
+    url = "https://api.github.com/repos/Homebrew/linuxbrew-core/dispatches"
+    GitHub.open_api(url, data: data, request_method: :POST, scopes: ["repo"])
+  end
+end
